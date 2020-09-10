589ce2c6817f4822e39ac27ec64aabb39a4ebdb2
Julia <julia.24@mail.ru>
2020-09-10 12:10:09 +0400

    section portfolio insertion 2

--- docs/js/main.js
+++ docs/js/main.js
@@ -4,26 +4,46 @@ $(document).ready(function () {
     const mobMenu = document.querySelector(".header-menu");
     const overlayEl = document.querySelector("#overlay");
 
+    //клик по иконке Гамбургер
     menuToggle.addEventListener("click", function () {
         this.classList.toggle("active");
         mobMenu.classList.toggle("active");
         overlayEl.classList.toggle("active");
   });
 
+  
 
+  let containerEl = document.querySelector("#portfolio-projects");
+  let mixer = mixitup(containerEl, {
+
+  classNames: {
+  block: ""
+  }
+});
+
+
+  //FORM PLACEHOLDER
+  const formInputs = document.querySelectorAll(".form-field");
+  // console.log(formInputs);
+  for(let item of formInputs) {
+    const thisPlaceholder = item.nextElementSibling;
+    // console.log(thisPlaceholder);
+
+    item.addEventListener("focus", function(){
+      thisPlaceholder.classList.add("active");
+  });
+
+  item.addEventListener("blur",function() {
+    if(item.value == "") {
+      thisPlaceholder.classList.remove("active");
+    }
+  });
+
+  }
 
 })
 
 
-// $(document).ready(function () {
-//   let containerEl = document.querySelector("#portfolio-projects");
-//   let mixer = mixitup(containerEl, {
-
-//   classNames: {
-//   block: ""
-//   }
-// });
-// })
 
 
 
